4242770cb022aa9520312fab89d8ba79f19517d4
Martin Aja <martinjoseaja@gmail.com>
2022-03-23 17:07:48 -0300

    Update Footer.js

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -1,10 +1,12 @@
+import "./footer.css";
+
 const Footer = () => {
-	return  <div className="footer">
-	<h6>Copyrigth 20022 All right registered</h6>
-	<p className='squad'>Squad Arquitecturra empresarial</p>
-	
-	
-</div>
+  return (
+    <div className="footer">
+      <h6>Copyrigth 20022 All right registered</h6>
+      <p className="squad">Squad Arquitecturra empresarial</p>
+    </div>
+  );
 };
 
 export default Footer;
