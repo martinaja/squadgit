adb72063bddefce7aff3cc8d55d104a0085bba2e
Martin Aja <martinjoseaja@gmail.com>
2022-03-25 19:47:12 -0300

    Refactored styles and login

--- src/components/home/Header/Header.js
+++ src/components/home/Header/Header.js
@@ -5,42 +5,44 @@ import './header.css';
 import { loginContext } from '../../../context/LoginContext';
 
 const Header = () => {
-	const login = useContext(loginContext);
-	const { open, setOpen, isLogged, setIsLogged } = login;
+    const login = useContext(loginContext);
+    const { open, setOpen, isLogged, setIsLogged, email } = login;
 
-	return (
-		<>
-			<header className="d-sm-flex justify-content-sm-end align-items-sm-center header">
-				<Modal isOpen={open} toggle={() => setOpen(!open)}> 
-					<ModalBody>
-						<Login />
-					</ModalBody>
-				</Modal>
-			{/* Cambiar el nombre del boton removiendo el login del localstorage*/}
-				{isLogged ? (
-					<button
-						onClick={() => { // Funcion que cierra la sesion
-							setIsLogged(false); // Seteo el estado de la sesion
-							localStorage.removeItem('login'); // Remuevo el login del localstorage
-						}}
-						className=" d-sm-flex justify-content-sm-center align-items-sm-center header"
-						type="button"
-					>
-						CERRAR SESIÓN
-					</button>
-				) : (
-					<button
-						onClick={() => setOpen(!open)} // Funcion que abre la sesion
-						className=" d-sm-flex justify-content-sm-center align-items-sm-center header"
-						type="button" 
-					>
-						INICIAR SESIÓN 
-						<i className="fa fa-user icon"></i> 
-					</button>
-				)}
-			</header>
-		</>
-	);
+    return (
+        <>
+            <header className="header">
+                <Modal isOpen={open} toggle={() => setOpen(!open)}>
+                    <ModalBody>
+                        <Login />
+                    </ModalBody>
+                </Modal>
+                {isLogged ? <p className="user">Bienvenido {email}</p> : <p className="user">Bienvenido Invitado</p>}
+                {/* Cambiar el nombre del boton removiendo el login del localstorage*/}
+                {isLogged ? (
+                    <button
+                        className="button-login"
+                        onClick={() => {
+                            // Funcion que cierra la sesion
+                            setIsLogged(false); // Seteo el estado de la sesion
+                            localStorage.removeItem('login'); // Remuevo el login del localstorage
+                        }}
+                        type="button"
+                    >
+                        CERRAR SESIÓN
+                    </button>
+                ) : (
+                    <button
+                        className="button-login"
+                        onClick={() => setOpen(!open)} // Funcion que abre la sesion
+                        type="button"
+                    >
+                        INICIAR SESIÓN
+                        <i className="fa fa-user icon"></i>
+                    </button>
+                )}
+            </header>
+        </>
+    );
 };
 
 export default Header;
